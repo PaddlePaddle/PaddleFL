74fda103b881e494d1fc275fa954f436bf1f8e50
qjing666 <jingqinghe@baidu.com>
2020-01-20 15:51:36 +0800

    make examples more easy to follow

--- paddle_fl/examples/submitter_demo/kill.sh
+++ paddle_fl/examples/submitter_demo/kill.sh
@@ -1,1 +1,3 @@
-/home/jingqinghe/mpi_feed4/smart_client/bin/qdel $1".yq01-hpc-lvliang01-smart-master.dmop.baidu.com"
+unset http_proxy
+unset https_proxy
+/home/jingqinghe/tools/mpi_feed4/smart_client/bin/qdel $1".yq01-hpc-lvliang01-smart-master.dmop.baidu.com"
