afe74b5dbd9c56d6988ad43fc8a36bf8383a188c
heya02 <heya02@baidu.com>
2020-07-28 03:07:55 +0000

    update run.sh and limit the epoch to train

--- python/paddle_fl/paddle_fl/examples/ctr_demo/run.sh
+++ python/paddle_fl/paddle_fl/examples/ctr_demo/run.sh
@@ -1,14 +1,16 @@
 unset http_proxy
 unset https_proxy
-python fl_master.py
+log_dir=${1:-$(pwd)}
+mkdir -p ${log_dir}
+python fl_master.py > ${log_dir}/master.log 2>&1 &
 sleep 2
-python -u fl_scheduler.py > scheduler.log &
+python -u fl_scheduler.py > ${log_dir}/scheduler.log 2>&1 &
 sleep 5
-python -u fl_server.py >server0.log &
+python -u fl_server.py > ${log_dir}/server0.log 2>&1 &
 sleep 2
 for ((i=0;i<2;i++))
 do 
-    python -u fl_trainer.py $i >trainer$i.log &
+    python -u fl_trainer.py $i > ${log_dir}/trainer$i.log 2>&1 &
     sleep 2
 done
 	
