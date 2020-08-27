0b003bc46d6881bbe1fee9a5fc4f9da0acda4728
jhjiangcs <jhjiangcs@gmail.com>
2020-08-27 10:04:48 +0000

    add lenet network demo, improve mnist demo.

---
+++ python/paddle_fl/mpc/examples/lenet_with_mnist/README.md
@@ -0,0 +1,94 @@
+## Instructions for PaddleFL-MPC MNIST Demo
+
+([简体中文](./README_CN.md)|English)
+
+This document introduces how to run MNIST demo based on Paddle-MPC, which has two ways of running, i.e., single machine and multi machines.
+
+### 1. Running on Single Machine
+
+#### (1). Prepare Data
+
+Copy scripts `../logistic_with_mnist/process_data.py` and `../logistic_with_mnist/decrypt_save.py` into this demo's directory `lenet_with_mnist`. Generate encrypted training and testing data utilizing `generate_encrypted_data()` and `generate_encrypted_test_data()` in `process_data.py` script. Users can run the script with command `python process_data.py` to generate encrypted feature and label in given directory, e.g., `./mpc_data/`. Users can specify `class_num` (2 or 10) to determine the encrypted data is for `fc_sigmoid`(two classes) or `lenet`(10 classes) network.  Different suffix names are used for these files to indicate the ownership of different computation parties. For instance, a file named `mnist2_feature.part0` means it is a feature file of party 0.
+
+#### (2). Launch Demo with A Shell Script
+
+You should set the env params as follow:
+
+```
+export PYTHON=/yor/python
+export PATH_TO_REDIS_BIN=/path/to/redis_bin
+export LOCALHOST=/your/localhost
+export REDIS_PORT=/your/redis/port
+```
+
+Launch demo with the `run_standalone.sh` script. The concrete command is:
+
+```bash
+bash run_standalone.sh train_fc_sigmoid.py
+```
+
+The information of current epoch and step will be displayed on screen while training, as well as the total cost time when traning finished.
+
+Besides, predictions would be made in this demo once training is finished. The predictions with cypher text format would be save in `./mpc_infer_data/` directory (users can modify it in the python script `train_fc_sigmoid.py`), and the format of file name is similar to what is described in Step 1.
+
+#### (3). Decrypt Data
+
+Decrypt the saved prediction data and save the decrypted prediction results into a specified file using `decrypt_data_to_file()` in `process_data.py` script. For example, users can write the following code into a python script named `decrypt_save.py`, and then run the script with command `python decrypt_save.py decrypt_file`. The decrypted prediction results would be saved into `decrypt_file`.
+
+```python
+import sys
+
+decrypt_file=sys.argv[1]
+import process_data
+process_data.decrypt_data_to_file("/tmp/mnist_output_prediction", (BATCH_SIZE,), decrypt_file)
+```
+
+
+### 2. Running on Multi Machines
+
+#### (1). Prepare Data
+
+Data owner encrypts data. Concrete operations are consistent with “Prepare Data” in “Running on Single Machine”.
+
+#### (2). Distribute Encrypted Data
+
+According to the suffix of file name, distribute encrypted data files to `./mpc_data/ ` directories of all 3 computation parties. For example, send `mnist2_feature.part0` and `mnist2_label.part0` to `./mpc_data/` of party 0 with `scp` command.
+
+#### (3). Modify mnist_demo.py
+
+Each computation party modifies `localhost` in the following code as the IP address of it's machine.
+
+```python
+pfl_mpc.init("aby3", int(role), "localhost", server, int(port))
+```
+
+#### (4). Launch Demo on Each Party
+
+**Note** that Redis service is necessary for demo running. Remember to clear the cache of Redis server before launching demo on each computation party, in order to avoid any negative influences caused by the cached records in Redis. The following command can be used for clear Redis, where REDIS_BIN is the executable binary of redis-cli, SERVER and PORT represent the IP and port of Redis server respectively.
+
+```
+$REDIS_BIN -h $SERVER -p $PORT flushall
+```
+
+Launch demo on each computation party with the following command,
+
+```
+$PYTHON_EXECUTABLE train_fc_sigmoid.py $PARTY_ID $SERVER $PORT
+```
+
+where PYTHON_EXECUTABLE is the python which installs PaddleFL, PARTY_ID is the ID of computation party, which is 0, 1, or 2, SERVER and PORT represent the IP and port of Redis server respectively.
+
+Similarly, predictions with cypher text format would be saved in `./mpc_infer_data/` directory, for example, a file named `mnist_output_prediction.part0` for party 0.
+
+#### (5). Decrypt Prediction Data
+
+Each computation party sends  `mnist_output_prediction.part` file in `./mpc_infer_data/` directory to the `./mpc_infer_data/` directory of data owner. Data owner decrypts the prediction data and saves the decrypted prediction results into a specified file using `decrypt_data_to_file()` in `process_data.py` script. For example, users can write the following code into a python script named `decrypt_save.py`, and then run the script with command `python decrypt_save.py decrypt_file`. The decrypted prediction results would be saved into file `decrypt_file`.
+
+```python
+import sys
+
+decrypt_file=sys.argv[1]
+import process_data
+process_data.decrypt_data_to_file("./mpc_infer_data/mnist_output_prediction", (BATCH_SIZE,), decrypt_file)
+```
+
---
+++ python/paddle_fl/mpc/examples/logistic_with_mnist/process_data.py
@@ -0,0 +1,173 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Process data for MNIST: 10 classes.
+"""
+import os
+import time
+import logging
+import numpy as np
+import six
+import paddle
+from paddle_fl.mpc.data_utils import aby3
+
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("fluid")
+logger.setLevel(logging.INFO)
+
+
+sample_reader = paddle.dataset.mnist.train()
+test_reader = paddle.dataset.mnist.test()
+
+
+def generate_encrypted_train_data(mpc_data_dir, class_num):
+    """
+    generate encrypted samples
+    """
+
+    def encrypted_mnist_features():
+        """
+        feature reader
+        """
+        for instance in sample_reader():
+            yield aby3.make_shares(instance[0])
+
+    def encrypted_mnist_labels():
+        """
+        label reader
+        """
+        for instance in sample_reader():
+            if class_num == 2:
+                label = np.array(1) if instance[1] == 0 else np.array(0)
+            elif class_num == 10:
+                label = np.eye(N=1, M=10, k=instance[1], dtype=float).reshape(10)
+            else:
+                raise ValueError("class_num should be 2 or 10, but received {}.".format(class_num))
+            yield aby3.make_shares(label)
+
+    aby3.save_aby3_shares(encrypted_mnist_features, mpc_data_dir + "mnist{}_feature".format(class_num))
+    aby3.save_aby3_shares(encrypted_mnist_labels, mpc_data_dir + "mnist{}_label".format(class_num))
+
+
+def generate_encrypted_test_data(mpc_data_dir, class_num, label_mnist_filepath):
+    """
+    generate encrypted samples
+    """
+
+    def encrypted_mnist_features():
+        """
+        feature reader
+        """
+        for instance in test_reader():
+            yield aby3.make_shares(instance[0])
+
+    def encrypted_mnist_labels():
+        """
+        label reader
+        """
+        for instance in test_reader():
+            if class_num == 2:
+                label = np.array(1) if instance[1] == 0 else np.array(0)
+                with open(label_mnist_filepath, 'a+') as f:
+                    f.write(str(1 if instance[1] == 0 else 0) + '\n')
+            elif class_num == 10:
+                label = np.eye(N=1, M=10, k=instance[1], dtype=float).reshape(10)
+                with open(label_mnist_filepath, 'a+') as f:
+                    f.write(str(instance[1]) + '\n')
+            else:
+                raise ValueError("class_num should be 2 or 10, but received {}.".format(class_num))
+            yield aby3.make_shares(label)
+
+    aby3.save_aby3_shares(encrypted_mnist_features, mpc_data_dir + "mnist{}_test_feature".format(class_num))
+    aby3.save_aby3_shares(encrypted_mnist_labels, mpc_data_dir + "mnist{}_test_label".format(class_num))
+
+
+def load_decrypt_data(filepath, shape):
+    """
+    load the encrypted data and reconstruct
+    """
+    part_readers = []
+    for id in six.moves.range(3):
+        part_readers.append(aby3.load_aby3_shares(filepath, id=id, shape=shape))
+    aby3_share_reader = paddle.reader.compose(part_readers[0], part_readers[1], part_readers[2])
+
+    for instance in aby3_share_reader():
+        p = aby3.reconstruct(np.array(instance))
+        logger.info(p)
+
+
+def load_decrypt_bs_data(filepath, shape):
+    """
+    load the encrypted data and reconstruct
+    """
+    part_readers = []
+    for id in six.moves.range(3):
+        part_readers.append(aby3.load_aby3_shares(filepath, id=id, shape=shape))
+    aby3_share_reader = paddle.reader.compose(part_readers[0], part_readers[1], part_readers[2])
+
+    for instance in aby3_share_reader():
+        p = np.bitwise_xor(np.array(instance[0]), np.array(instance[1]))
+        p = np.bitwise_xor(p, np.array(instance[2]))
+        logger.info(p)
+
+
+def decrypt_data_to_file(filepath, shape, decrypted_filepath):
+    """
+    load the encrypted data (arithmetic share) and reconstruct to a file
+    """
+    if os.path.exists(decrypted_filepath):
+        os.remove(decrypted_filepath)
+    part_readers = []
+    for id in six.moves.range(3):
+        part_readers.append(aby3.load_aby3_shares(filepath, id=id, shape=shape))
+    aby3_share_reader = paddle.reader.compose(part_readers[0], part_readers[1], part_readers[2])
+
+    for instance in aby3_share_reader():
+        p = aby3.reconstruct(np.array(instance))
+        with open(decrypted_filepath, 'a+') as f:
+            for i in p:
+                f.write(str(np.argmax(i)) + '\n')
+
+
+def decrypt_bs_data_to_file(filepath, shape, decrypted_filepath):
+    """
+    load the encrypted data (boolean share) and reconstruct to a file
+    """
+    if os.path.exists(decrypted_filepath):
+        os.remove(decrypted_filepath)
+    part_readers = []
+    for id in six.moves.range(3):
+        part_readers.append(aby3.load_aby3_shares(filepath, id=id, shape=shape))
+    aby3_share_reader = paddle.reader.compose(part_readers[0], part_readers[1], part_readers[2])
+
+    for instance in aby3_share_reader():
+        p = np.bitwise_xor(np.array(instance[0]), np.array(instance[1]))
+        p = np.bitwise_xor(p, np.array(instance[2]))
+        with open(decrypted_filepath, 'a+') as f:
+            for i in p:
+                f.write(str(i) + '\n')
+
+
+if __name__ == '__main__':
+    mpc_data_dir = './mpc_data/'
+    label_mnist_filepath = mpc_data_dir + "label_mnist"
+    if not os.path.exists(mpc_data_dir):
+        os.mkdir(mpc_data_dir)
+    if os.path.exists(label_mnist_filepath):
+        os.remove(label_mnist_filepath)
+
+    class_num = 2
+    generate_encrypted_train_data(mpc_data_dir, class_num)
+    generate_encrypted_test_data(mpc_data_dir, class_num, label_mnist_filepath)
