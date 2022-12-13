da0eb19f753683380d0efd352cb179f6f9c6b232
zhonglei.han <hzl@bupt.edu.cn>
2022-12-14 00:40:46 +0800

    Added two demos for calculating the maximum of two parties or three parties.

--- python/paddle_fl/mpc/tests/demo/variance/variance2/data.py
+++
@@ -1,9 +0,0 @@
-import random
-
-file_handle1=open('Input-P0.list',mode='w')
-for i in range(9):
-    file_handle1.write(str(round(random.random(),6)) + '\n')
-
-file_handle1=open('Input-P1.list',mode='w')
-for i in range(10):
-    file_handle1.write(str(round(random.random(),6)) + '\n')
