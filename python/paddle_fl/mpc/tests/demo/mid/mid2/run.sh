5f9f334e5ba51ef9b4b5c79c208447318eeb4716
tianyi.yue <yuety@bupt.edu.cn>
2022-12-13 12:42:32 +0800

    Added a demo for calculating the median of two parties.

--- python/paddle_fl/mpc/tests/demo/mid/mid2/run.sh
+++
@@ -1,5 +0,0 @@
-export TEST_REDIS_IP="127.0.0.1"
-export TEST_REDIS_PORT="6379"
-nohup python3 mid2_C0.py >> ./logs/console0.log 2>>./logs/error0.log &
-nohup python3 mid2_C1.py >> ./logs/console1.log 2>>./logs/error1.log &
-nohup python3 mid2_C2.py >> ./logs/console2.log 2>>./logs/error2.log &
